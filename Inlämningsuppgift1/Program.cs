1bb70e02e81c6db5aa6d5c359274b499a75b7573
Sanjin Ajanic <sanjin.ajanic@hotmail.com>
2020-11-19 10:04:19 +0100

    Add project files.

---
+++ Inlämningsuppgift1/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+
+namespace Inlämningsuppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a Operator:");
+            string op = Console.ReadLine();
+
+
+
+            Console.Write("Enter a number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.Write("Enter second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
