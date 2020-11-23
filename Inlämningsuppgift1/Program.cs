ffe591e2b382fa57c6a3a74324da414df61e58b9
Sanjin Ajanic <sanjin.ajanic@hotmail.com>
2020-11-23 14:45:40 +0100

    tagit bort en operator och lagt till resultat summan i lista

--- Inlämningsuppgift1/Program.cs
+++ Inlämningsuppgift1/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Collections.Generic;
 
 namespace Inlämningsuppgift1
 {
@@ -11,9 +12,6 @@ namespace Inlämningsuppgift1
             Console.Write("Enter a Operator:");
             string op1 = Console.ReadLine();
 
-            Console.Write("Enter secound Operator:");
-            string op2 = Console.ReadLine();
-
             Console.Write("Enter a number:");
             double num1 = Convert.ToDouble(Console.ReadLine());
 
@@ -21,46 +19,49 @@ namespace Inlämningsuppgift1
             Console.Write("Enter second number:");
             double num2 = Convert.ToDouble(Console.ReadLine());
 
-            double result;
-
+            List<double> result = new List<double>();
+            int counter = default(int);
 
             if (op1 == "+")
             {
-                Console.WriteLine("Resultatet är: " + (result = num1 + num2) + " (" + num1 + "+" + num2 + ")");
+                result.Add(num1 + num2);
+                counter = result.Count - 1;
+                //console.writeline("resultatet är: " + (result = num1 + num2) + " (" + num1 + "+" + num2 + ")");
+                Console.WriteLine("{0} {1} {2} = {3}", num1, op1, num2, result[counter]);
             }
-            else if (op1 == "-")
-            {
-                Console.WriteLine("Resultatet är: " +  (result = num1 - num2) + " (" + num1 + "-" + num2 + ")");
-            }
-            else if (op1 == "/")
-            {
-                Console.WriteLine("Resultatet är: " + (result = num1 / num2) + " (" + num1 + "/" + num2 + ")");
-            }
-            else if (op1 == "*")
-            {
-                Console.WriteLine("Resultatet är: " + (result = num1 * num2) + " (" + num1 + "*" + num2 + ")");
-            }
+            //else if (op1 == "-")
+            //{
+            //    Console.WriteLine("Resultatet är: " +  (result = num1 - num2) + " (" + num1 + "-" + num2 + ")");
+            //}
+            //else if (op1 == "/")
+            //{
+            //    Console.WriteLine("Resultatet är: " + (result = num1 / num2) + " (" + num1 + "/" + num2 + ")");
+            //}
+            //else if (op1 == "*")
+            //{
+            //    Console.WriteLine("Resultatet är: " + (result = num1 * num2) + " (" + num1 + "*" + num2 + ")");
+            //}
            
-            if (op2 == "+")
-            {
-                Console.WriteLine("Resultatet är: " + (result = num1 + num2) + " (" + num1 + "+" + num2 + ")");
-            }
-            else if (op2 == "-")
-            {
-                Console.WriteLine("Resultatet är: " + (result = num1 - num2) + " (" + num1 + "-" + num2 + ")");
-            }
-            else if (op2 == "/")
-            {
-                Console.WriteLine("Resultatet är: " + (result = num1 / num2) + " (" + num1 + "/" + num2 + ")");
-            }
-            else if (op2 == "*")
-            {
-                Console.WriteLine("Resultatet är: " + (result = num1 * num2) + " (" + num1 + "*" + num2 +  ")");
-            }
+            //if (op2 == "+")
+            //{
+            //    Console.WriteLine("Resultatet är: " + (result = num1 + num2) + " (" + num1 + "+" + num2 + ")");
+            //}
+            //else if (op2 == "-")
+            //{
+            //    Console.WriteLine("Resultatet är: " + (result = num1 - num2) + " (" + num1 + "-" + num2 + ")");
+            //}
+            //else if (op2 == "/")
+            //{
+            //    Console.WriteLine("Resultatet är: " + (result = num1 / num2) + " (" + num1 + "/" + num2 + ")");
+            //}
+            //else if (op2 == "*")
+            //{
+            //    Console.WriteLine("Resultatet är: " + (result = num1 * num2) + " (" + num1 + "*" + num2 +  ")");
+            //}
 
 
 
-            Console.WriteLine(op1 + op2 + num1 + num2 );
+            
 
 
 
