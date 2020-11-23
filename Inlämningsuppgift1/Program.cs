06f36529b2ec74f772410d8d3f4a1f6b235ab597
Sanjin Ajanic <sanjin.ajanic@hotmail.com>
2020-11-23 15:50:06 +0100

    skapat möjlighet för användaren att avsluta program och skapat en loop som loopar om programet sålänge användaren inte har valt att avsluta.

--- Inlämningsuppgift1/Program.cs
+++ Inlämningsuppgift1/Program.cs
@@ -8,65 +8,84 @@ namespace Inlämningsuppgift1
     {
         static void Main(string[] args)
         {
-
-            Console.Write("Enter a Operator:");
-            string op1 = Console.ReadLine();
-
-            Console.Write("Enter a number:");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-
-
-            Console.Write("Enter second number:");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-
             List<double> result = new List<double>();
             int counter = default(int);
+            bool quit = default(bool);
 
-            if (op1 == "+")
+            do
             {
-                result.Add(num1 + num2);
-                counter = result.Count - 1;
-                //console.writeline("resultatet är: " + (result = num1 + num2) + " (" + num1 + "+" + num2 + ")");
-                Console.WriteLine("{0} {1} {2} = {3}", num1, op1, num2, result[counter]);
-            }
-            //else if (op1 == "-")
-            //{
-            //    Console.WriteLine("Resultatet är: " +  (result = num1 - num2) + " (" + num1 + "-" + num2 + ")");
-            //}
-            //else if (op1 == "/")
-            //{
-            //    Console.WriteLine("Resultatet är: " + (result = num1 / num2) + " (" + num1 + "/" + num2 + ")");
-            //}
-            //else if (op1 == "*")
-            //{
-            //    Console.WriteLine("Resultatet är: " + (result = num1 * num2) + " (" + num1 + "*" + num2 + ")");
-            //}
+
+                
+
+
+                Console.Write("Enter a Operator or a to quit program:");
+                string op1 = Console.ReadLine();
+
+                if (op1 == "a")
+                {
+                    quit = true;
+                    Console.Write("Du väljer att avsluta programet");
+                }
+                else
+                {
+
+
+
+                    Console.Write("Enter a number:");
+                    double num1 = Convert.ToDouble(Console.ReadLine());
+
+
+                    Console.Write("Enter second number:");
+                    double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+                    if (op1 == "+")
+                    {
+                        result.Add(num1 + num2);
+                        counter = result.Count - 1;
+
+                        Console.WriteLine("{0} {1} {2} = {3}", num1, op1, num2, result[counter]);
+                    }
+                    else if (op1 == "-")
+                    {
+                        result.Add(num1 - num2);
+                        counter = result.Count - 1;
+
+                        Console.WriteLine("{0} {1} {2} = {3}", num1, op1, num2, result[counter]);
+                    }
+                    else if (op1 == "/")
+                    {
+                        result.Add(num1 / num2);
+                        counter = result.Count - 1;
+
+                        Console.WriteLine("{0} {1} {2} = {3}", num1, op1, num2, result[counter]);
+                    }
+                    else if (op1 == "*")
+                    {
+                        result.Add(num1 * num2);
+                        counter = result.Count - 1;
+
+                        Console.WriteLine("{0} {1} {2} = {3}", num1, op1, num2, result[counter]);
+                    }
+
+
+                }
+                Console.ReadLine();
+
+            } while (!quit);
            
-            //if (op2 == "+")
-            //{
-            //    Console.WriteLine("Resultatet är: " + (result = num1 + num2) + " (" + num1 + "+" + num2 + ")");
-            //}
-            //else if (op2 == "-")
-            //{
-            //    Console.WriteLine("Resultatet är: " + (result = num1 - num2) + " (" + num1 + "-" + num2 + ")");
-            //}
-            //else if (op2 == "/")
-            //{
-            //    Console.WriteLine("Resultatet är: " + (result = num1 / num2) + " (" + num1 + "/" + num2 + ")");
-            //}
-            //else if (op2 == "*")
-            //{
-            //    Console.WriteLine("Resultatet är: " + (result = num1 * num2) + " (" + num1 + "*" + num2 +  ")");
-            //}
+            double totalSum = 0;
+            foreach (var item in result)
+            {
+                totalSum += item;
+            }
 
-
+            Console.WriteLine(totalSum + " Är summan av dina beräkningar");
 
             
 
-
-
-
-            Console.ReadLine();
+            
         }
     }
 }
