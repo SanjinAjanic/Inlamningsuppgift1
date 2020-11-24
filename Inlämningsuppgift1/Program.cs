84e44fbec6d34bc7e59e072894559ab4d56cce95
Sanjin Ajanic <sanjin.ajanic@hotmail.com>
2020-11-24 23:01:30 +0100

    fixat att operator 2 funkar som den ska

--- Inlämningsuppgift1/Program.cs
+++ Inlämningsuppgift1/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace Inlämningsuppgift1
@@ -9,27 +8,30 @@ namespace Inlämningsuppgift1
         static void Main(string[] args)
         {
 
-             
+
+
             Console.Title = "Sanjin Inlämningsuppgift1";
             Console.BackgroundColor
            = ConsoleColor.Blue;
 
 
+            Console.Clear();
+
             List<double> result = new List<double>();
-            int counter = default(int);
-            bool quit = default(bool);
+            int counter = 0;
+            bool quit = false;
 
 
             do
             {
 
-                
 
 
-                Console.Write("Enter a Operator or a to quit program:");
+
+                Console.Write("Enter an Operator or NO to quit program:");
                 string op1 = Console.ReadLine();
 
-                if (op1 == "a")
+                if (op1.ToLower() == "no")
                 {
                     quit = true;
                     Console.Write("Du väljer att avsluta programet");
@@ -37,6 +39,8 @@ namespace Inlämningsuppgift1
                 else
                 {
 
+                    Console.Write("Enter secound Operator:");
+                    string op2 = Console.ReadLine();
 
 
                     Console.Write("Enter a number:");
@@ -48,86 +52,153 @@ namespace Inlämningsuppgift1
                     Console.Write("Enter second number:");
                     double num2 = Convert.ToDouble(Console.ReadLine());
 
+                    Console.Write("Enter thrd number:");
+                    double num3 = Convert.ToDouble(Console.ReadLine());
 
-                    
+
 
 
 
                     if (op1 == "+")
                     {
-                        result.Add(num1 + num2);
+
+
+                        if (op2 == "+")
+                        {
+                            result.Add(num1 + num2 + num3);
+                        }
+                        else if (op2 == "-")
+                        {
+                            result.Add(num1 + num2 - num3);
+                        }
+                        else if (op2 == "*")
+                        {
+                            result.Add(num1 + num2 * num3);
+                        }
+                        else if (op2 == "/")
+                        {
+                            result.Add(num1 + num2 / num3);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Operator two is a invalid operator");
+                        }
                         counter = result.Count - 1;
 
-                        Console.WriteLine("{0} {1} {2} = {3}", num1, op1, num2, result[counter]);
-
+                        Console.WriteLine("{0} {1} {2} {3} {4} = {5}", num1, op1, num2, op2, num3, result[counter]);
 
                     }
                     else if (op1 == "-")
                     {
-                        result.Add(num1 - num2);
+                        if (op2 == "+")
+                        {
+                            result.Add(num1 - num2 + num3);
+                        }
+                        else if (op2 == "-")
+                        {
+                            result.Add(num1 - num2 - num3);
+                        }
+                        else if (op2 == "*")
+                        {
+                            result.Add(num1 - num2 * num3);
+                        }
+                        else if (op2 == "/")
+                        {
+                            result.Add(num1 - num2 / num3);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Operator two is a invalid operator");
+                        }
                         counter = result.Count - 1;
 
-                        Console.WriteLine("{0} {1} {2} = {3}", num1, op1, num2, result[counter]);
-                    }
-                    else if (op1 == "/")
-                    {
-                        result.Add(num1 / num2);
-                        counter = result.Count - 1;
+                        Console.WriteLine("{0} {1} {2} {3} {4} = {5}", num1, op1, num2, op2, num3, result[counter]);
 
-                        Console.WriteLine("{0} {1} {2} = {3}", num1, op1, num2, result[counter]);
                     }
                     else if (op1 == "*")
                     {
-                        result.Add(num1 * num2);
+                        if (op2 == "+")
+                        {
+                            result.Add(num1 * num2 + num3);
+                        }
+                        else if (op2 == "-")
+                        {
+                            result.Add(num1 * num2 - num3);
+                        }
+                        else if (op2 == "*")
+                        {
+                            result.Add(num1 * num2 * num3);
+                        }
+                        else if (op2 == "/")
+                        {
+                            result.Add(num1 * num2 / num3);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Operator two is a invalid operator");
+                        }
                         counter = result.Count - 1;
 
-                        Console.WriteLine("{0} {1} {2} = {3}", num1, op1, num2, result[counter]);
+                        Console.WriteLine("{0} {1} {2} {3} {4} = {5}", num1, op1, num2, op2, num3, result[counter]);
+                    }
+                    else if (op1 == "/")
+                    {
+                        if (op2 == "+")
+                        {
+                            result.Add(num1 / num2 + num3);
+                        }
+                        else if (op2 == "-")
+                        {
+                            result.Add(num1 / num2 - num3);
+                        }
+                        else if (op2 == "*")
+                        {
+                            result.Add(num1 / num2 * num3);
+                        }
+                        else if (op2 == "/")
+                        {
+                            result.Add(num1 / num2 / num3);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Operator two is a invalid operator");
+                        }
+                        counter = result.Count - 1;
+
+                        Console.WriteLine("{0} {1} {2} {3} {4} = {5}", num1, op1, num2, op2, num3, result[counter]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Operator one is a invalid operator");
 
                     }
 
-                  
 
 
 
+
+
+                    double totalSum = 0;
+                    foreach (var item in result)
+                    {
+                        totalSum += item;
+                    }
+
+
+                    if (totalSum < 100)
+                    {
+                        Console.WriteLine("Less then a hundred");
+                    }
+                    else if (totalSum > 100)
+                    {
+                        Console.WriteLine("More then a hundred");
+                    }
+                    else if (totalSum == 100)
+                    {
+                        Console.WriteLine("Cool, now you have a hundred, clap clap");
+                    }
                 }
-                Console.ReadLine();
-
-            } while (!quit);
-
-           
-            double totalSum = 0;
-            foreach (var item in result)
-            {
-                totalSum += item;
-            }
-          
-            
-            if (totalSum < 100)
-            {
-                Console.WriteLine("Less then a hundred");
-            }
-            else if (totalSum > 100)
-            {
-                Console.WriteLine("More then a hundred");
-            }
-            else if (totalSum == 100)
-            {
-                Console.WriteLine("Cool, now you have a hundred, clap clap");
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            } while (quit == false);
         }
     }
 }
